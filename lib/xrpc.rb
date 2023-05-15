3983c7925525e063cd361460a26f56a2ea3dcdff
ShreyanJain9 <shreyan.jain.9@outlook.com>
2023-05-15 16:34:42 -0700

    directly do XRPC.request

--- lib/xrpc.rb
+++ lib/xrpc.rb
@@ -4,6 +4,16 @@ require "httparty"
 require "json"
 
 module XRPC
+  def request(pds, endpoint_location, *params)
+    query_params = URI.encode_www_form(*params)
+    @request_uri = URI("#{pds}/xrpc/#{endpoint_location}?#{query_params}")
+
+    response = HTTParty.get(@request_uri)
+    JSON.parse(response.body)
+  end
+
+  module_function :request
+
   class Endpoint
     attr_reader :request_uri
 
