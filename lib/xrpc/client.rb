07d878fdf1bd5b2f2f0362c59b01b308a1f5e31f
ShreyanJain9 <shreyan.jain.9@outlook.com>
2023-11-01 16:40:33 -0700

    more natural client interface

--- lib/xrpc/client.rb
+++ lib/xrpc/client.rb
@@ -9,7 +9,8 @@ module XRPC
   end
 
   class Client
-    attr_accessor :headers, :token
+    attr_accessor :headers
+    attr_reader :token, :base_url
 
     def initialize(base_url, token = nil)
       @token = token
@@ -18,7 +19,12 @@ module XRPC
     end
 
     def headers!
-      @headers = { :"Content-Type" => "application/json", :Authorization => "Bearer #{@token}" }
+      @headers[:Authorization] = "Bearer #{@token}"
+    end
+
+    def token=(token)
+      @token = token
+      headers!
     end
 
     def get
@@ -37,12 +43,13 @@ module XRPC
       include HTTParty
 
       def initialize(base_url, headers)
-        self.class.base_uri base_url
+        @base_uri = base_url
         @headers = headers
       end
 
       def method_missing(method_name, **params)
-        self.class.get("/xrpc/#{method_name.to_s.gsub("_", ".")}", query: params, headers: @headers).then do |response|
+        return XRPC::Endpoint.new(self, method_name.to_s, :get) if method_name.to_s.gsub("_", ".") == method_name.to_s
+        self.class.get("#{@base_uri}/xrpc/#{method_name.to_s.gsub("_", ".")}", query: params, headers: @headers).then do |response|
           response.body.empty? ? response.code : JSON.parse(response.body)
         end
       end
@@ -61,6 +68,28 @@ module XRPC
           response.body.empty? ? response.code : JSON.parse(response.body)
         end
       end
+    end
+  end
+end
+
+module XRPC
+  class Endpoint < Struct.new(:client, :lexicon, :type)
+    def call(**kwargs)
+      client.send(lexicon.gsub(".", "_"), **kwargs)
+    end
+
+    alias_method :[], :call
+
+    def to_proc
+      method(:call).to_proc
+    end
+
+    def |(hash)
+      call(**hash)
+    end
+
+    def method_missing(method_name, **kwargs)
+      Endpoint.new(client, "#{lexicon}.#{method_name}", type)
     end
   end
 end
